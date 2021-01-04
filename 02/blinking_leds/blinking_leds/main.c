4f22ed8cc0f1b81ec3978d19ef9ee7b01e00082f
Abdulrahman Ghanem <abdoghanem160@gmail.com>
2021-01-04 16:15:34 +0200

    Add the rest of Proteus files

--- 02/blinking_leds/blinking_leds/main.c
+++ 02/blinking_leds/blinking_leds/main.c
@@ -2,17 +2,15 @@
  * blinking_leds.c
  *
  * Created: 1/4/2021 9:12:00 AM
- * Author : Abu
- */ 
+ * Author : AbulrhmnGhanem
+ */
 
 #include <avr/io.h>
-
 
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
+    while (1)
     {
     }
 }
-
