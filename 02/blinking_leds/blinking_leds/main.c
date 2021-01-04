3c76c3504016c63021e133984da8a77160ecf235
Abdulrahman Ghanem <abdoghanem160@gmail.com>
2021-01-04 09:49:14 +0200

    Add prooject configuration files
    - Make atmel studio work with VSCode by pointing the right(avr-gcc)
    compiler.

---
+++ 02/blinking_leds/blinking_leds/main.c
@@ -0,0 +1,18 @@
+/*
+ * blinking_leds.c
+ *
+ * Created: 1/4/2021 9:12:00 AM
+ * Author : Abu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
