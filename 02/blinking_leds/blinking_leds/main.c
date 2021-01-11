8d64bb6a2050c4ef2161f699a49bca29aa00040e
Abdulrahman Ghanem <abdoghanem160@gmail.com>
2021-01-11 13:13:24 +0200

    02 - Naive implementation.
    - Just check if the pins are mapped correctly.

--- 02/blinking_leds/blinking_leds/main.c
+++ 02/blinking_leds/blinking_leds/main.c
@@ -16,6 +16,15 @@ int main(void)
     DDRD = 0b00001100;
     while (1)
     {
+        PORTA = 0b00000010;
+        _delay_ms(1000);
+        PORTA = 0b00000100;
+        _delay_ms(1000);
+        PORTA = 0b00000000;
+        PORTD = 0b00000100;
+        _delay_ms(1000);
+        PORTD = 0b00001000;
+        _delay_ms(1000);
         PORTA = 0b00000110;
         PORTD = 0b00001100;
         _delay_ms(1000);
