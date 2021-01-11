513b9aba8b5c4bbda4b8f956fbd3cdf024416634
Abdulrahman Ghanem <abdoghanem160@gmail.com>
2021-01-11 14:27:24 +0200

    Create 03 Atmel solution files.

---
+++ 03/7-seg/7-seg/main.c
@@ -0,0 +1,17 @@
+/*
+ * 7-seg.c
+ *
+ * Created: 1/11/2021 2:22:23 PM
+ * Author : AbulrhmnGhanem
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
---
+++ 03/LCD/LCD/main.c
@@ -0,0 +1,17 @@
+/*
+ * LCD.c
+ *
+ * Created: 1/11/2021 2:23:20 PM
+ * Author : AbulrhmnGhanem
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
