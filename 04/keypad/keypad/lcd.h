81b9e2f54bf26f49a862cee15cc0a2f96fe2ba84
Abdulrahman Ghanem <abdoghanem160@gmail.com>
2021-01-12 18:21:12 +0200

    Add keypad Atmel Solution

---
+++ 04/keypad/keypad/lcd.h
@@ -0,0 +1,26 @@
+/*
+ * lcd.h
+ *
+ * Created: 10/12/2019 7:15:13 PM
+ *  Author: moram
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LCD_DATA_PORT PORTB
+#define LCD_CONTROL_PORT PORTA
+#define LCD_RS PA1 //PORTA1
+#define LCD_EN PA0 //PORTA0
+
+void lcd_data_write(char data);
+void lcd_string_write(char *string);
+void lcd_command_write(char command);
+void lcd_init();
+
+#endif /* LCD_H_ */
